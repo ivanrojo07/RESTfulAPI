429947d2ff2b8c0d410c4cb9bf4cf8ea5d757a68
Ivan Rojo <ivanrojo07@gmail.com>
2017-06-16 18:04:53 -0500

    Creando los modelos

---
+++ app/Fabricante.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Fabricante extends Model
+{
+    //
+    protected $table = 'fabricantes';
+
+    protected $fillable = ['nombre', 'telefono'];
+
+    public function vehiculos ()
+    {
+    	$this->hasMany('Vehiculo');
+    }
+
+}
---
+++ app/Vehiculo.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Vehiculo extends Model
+{
+    //
+    protected $table = 'vehiculos';
+
+    protected $primaryKey = 'serie';
+
+    protected $fillable = [ 'color', 'cilindraje', 'potencia', 'peso'];
+
+    public function fabricante()
+    {
+    	$this->belongsTo('Fabricante');
+    }
+}
